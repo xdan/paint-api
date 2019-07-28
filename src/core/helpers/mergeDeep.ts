6b53b2f19ca0de7dfba51bcd027fbce8bc95d8b0
Valera <v-chupurnov@edadeal.ru>
2019-07-29 01:13:26 +0300

    Added deepMerge options

---
+++ src/core/helpers/mergeDeep.ts
@@ -0,0 +1,25 @@
+import { isDictionary } from "./index";
+import { IDictionary } from "../../types";
+
+export function mergeDeep(...objects: unknown[]) {
+	return objects.reduce<IDictionary>((prev, obj) => {
+		if (isDictionary(obj)) {
+			Object.keys(obj).forEach((key) => {
+				const pVal = prev[key];
+				const oVal = obj[key];
+
+				if (Array.isArray(pVal) && Array.isArray(oVal)) {
+					prev[key] = pVal.concat(...oVal);
+				}
+				else if (isDictionary(pVal) && isDictionary(oVal)) {
+					prev[key] = mergeDeep(pVal, oVal);
+				}
+				else {
+					prev[key] = oVal;
+				}
+			});
+		}
+
+		return prev;
+	}, {});
+}
